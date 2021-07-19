45dbfb9e6b4bdc3aea2ade8bf5862ba918117f6c
pulkitagrawal014 <pulkitagrawal147@gmail.com>
2021-07-19 18:08:42 +0530

    Initial commit

---
+++ src/MyComponents/About.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+export const About = () => {
+  return (
+    <div>
+      This is an about MyComponents
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Pariatur optio,
+        commodi hic ex alias cumque. Tempore explicabo modi corrupti consequatur
+        aut quis! Eveniet voluptates minima officiis omnis numquam quas,
+        sapiente a facere ad dicta?
+      </p>
+    </div>
+  );
+};
