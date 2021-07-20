3ca00dc423cc70f252527b8cca54e06705d72e9f
pulkitagrawal014 <pulkitagrawal147@gmail.com>
2021-07-20 13:49:51 +0530

    Initial commit

--- src/App.js
+++ src/App.js
@@ -6,6 +6,8 @@ import React, { useState, useEffect } from "react";
 import { AddTodo } from "./MyComponents/AddTodo";
 import { About } from "./MyComponents/About";
 import { BrowserRouter as Router, Switch, Route} from "react-router-dom";
+
+
 function App() {
   let initTodo;
   if (localStorage.getItem("todos") === null) {
@@ -13,11 +15,9 @@ function App() {
   } else {
     initTodo = JSON.parse(localStorage.getItem("todos"));
   }
+
   const onDelete = (todo) => {
     console.log("I am onDelete of todo", todo);
-    // Deleting this way in react does not work
-    // let index = todos.indexOf(todo);
-    // todos.splice(index, 1);
 
     setTodos(
       todos.filter((e) => {
@@ -32,9 +32,11 @@ function App() {
     let sno;
     if (todos.length === 0) {
       sno = 0;
-    } else {
+    } 
+    else {
       sno = todos[todos.length - 1].sno + 1;
     }
+
     const myTodo = {
       sno: sno,
       title: title,
